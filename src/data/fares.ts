6df68ff49865456c35d9fe0383630627524b2824
k-metra <metrakurt@gmail.com>
2026-08-17 20:57:25 +0800

    Initial commit

---
+++ src/data/fares.ts
@@ -0,0 +1,11 @@
+export const fareRules = {
+    regular:  {
+        minimum: 13,
+        increment: 2
+    },
+
+    senior: {
+        minimum: 11,
+        increment: 2
+    }
+};
---
+++ src/data/routes.ts
@@ -0,0 +1,40 @@
+export const routes = [
+    {
+        id: "malolos-bulakan",
+        name: "Malolos - Bulakan",
+        stops: [
+            "Bagumbayan/San Jose",
+            "Maysantol",
+            "San Nicolas",
+            "Pitpitan",
+            "Mambog",
+            "Matimbo",
+            "Panasahan",
+            "Bagna",
+            "Atlag",
+            "San Juan/Sto. Rosario"
+        ],
+    },
+
+    {
+        id: "guiguinto-bulakan",
+        name: "Guiguinto - Bulakan",
+        stops: [
+            "Bagumbayan/San Jose",
+            "Matungao",
+            "Tuktukan"
+        ],
+    },
+
+    {
+        id: "balagtas-bulakan",
+        name: "Balagtas - Bulakan",
+        stops: [
+            "Bagumbayan/San Jose",
+            "Matungao",
+            "Panginay Guiguinto",
+            "Panginay Balagtas",
+            "Wawa"
+        ],
+    },
+];
