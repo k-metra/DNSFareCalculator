b23fc69b2ecb1bc4672c4f8e555b842abe614e92
k-metra <metrakurt@gmail.com>
2026-08-17 23:41:22 +0800

    adjust header size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,9 @@ import logo from '../assets/logo.jpg';
 
 export default function Header() {
     return (
-        <header className="w-screen bg-background border-b border-b-white/25 min-h-32 flex items-center justify-center">
-            <img src={logo} alt="Logo" className="h-32" />  
+        <header className="w-full bg-background border-b border-b-border min-h-24 flex items-center justify-center gap-5 shadow-lg">
+            <img src={logo} alt="Logo" className="h-28" /> 
+            <h1 className="text-3xl font-semibold text-white font-main">Fare Calculator</h1> 
         </header>
 
     )
