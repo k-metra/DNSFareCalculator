5c280440092d384bf4a071debefa0d2ea47ba263
k-metra <metrakurt@gmail.com>
2026-08-17 23:40:47 +0800

    fix(dropdown): add max height and modify scrollbar thumb to be thin

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,29 +1,51 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { motion, AnimatePresence} from 'motion/react';
+
+import { type option } from '@utils/types';
 
 export type DropdownProps = {
-     options: string[];
-     value: string;
+     options: option[];
+     value?: string
      onChange: (value: string) => void;
      label: string;
+     disabled?: boolean;
 }
 
-export default function Dropdown({ DropdownProps }: { DropdownProps: DropdownProps }) {
+export default function Dropdown({ label, value, options, onChange, disabled = false }: DropdownProps ) {
     const [open, setOpen] = useState(false);
-    const { options, value, onChange, label } = DropdownProps;
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
-    const selectedOption = options.find((option) => option === value);
+    const handleClick = () => {
+        if (disabled) return;
+        setOpen((prev) => !prev);
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                    setOpen(false);
+                }
+            
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
 
     return (
-        <div className="relative w-full">
+        <div className="relative w-full" ref={dropdownRef}>
             <button
                 type="button"
-                onClick={() => setOpen(!open)}
-                className={`relative w-full h-14 px-4 border border-border rounded-md bg-background text-white text-left transition-colors
-                        ${open ? "border-blue-500" : "hover:border-gray-400"}
+                onClick={handleClick}
+                className={`relative w-full h-14 px-4 border border-border rounded-md bg-background text-white text-left transition-colors ${!disabled && "cursor-pointer"}
+                        ${open ? "border-blue-500" : "hover:border-gray-400}"}
                     `}
             >
                 <span
                     className={`
+                        font-main
                         absolute left-3 px-1
                         bg-background
                         text-sm
@@ -33,11 +55,41 @@ export default function Dropdown({ DropdownProps }: { DropdownProps: DropdownPro
                             : "top-1/2 -translate-y-1/2 text-gray-400"
                         }`}
                 >{label}</span>
+
+                <span className="font-main block truncate">
+                    {(options.find((option: option) => option.value === value)?.label)}
+                </span>
                 
                 <span className="absolute right-4 top-1/2 -translate-y-1/2">
                     ▼
                 </span>
+
             </button>
+                <AnimatePresence>
+                    {open && (
+                        <motion.div
+                            initial={{ opacity: 0, y: -5}}
+                            animate={{ opacity: 1, y: 0}}
+                            exit={{ opacity: 0, y: -5}}
+                            transition={{ duration: 0.15}}
+                            className="absolute z-50 mt-1 left-0 w-full scrollbar- scrollbar-thumb-border scrollbar-track-transparent scrollbar-thin max-h-48 scroll-m-9 rounded-md border border-border bg-background shadow-lg overflow-y-auto"
+                        >
+                            {
+                                options.map((option) => (
+                                    <button
+                                        key={option.label}
+                                        type="button"
+                                        onClick={() => {
+                                            onChange(option.value);
+                                            setOpen(false);
+                                        }}
+                                        className="font-main font-medium w-full px-4 py-3 text-left text-white hover:bg-white/10 transition-colors cursor-pointer"
+                                    >{option.label}</button>
+                                ))
+                            }
+                        </motion.div>
+                    )}
+                </AnimatePresence>
         </div>
     )
 }
