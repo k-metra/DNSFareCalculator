e60512796700a2ac3f922f4ce5494394214ebb30
k-metra <metrakurt@gmail.com>
2026-08-21 00:46:25 +0800

    fix(CalculationCard): prefix fares with peso sign

--- src/components/calculationCard.tsx
+++ src/components/calculationCard.tsx
@@ -50,11 +50,11 @@ export default function CalculationCard({
             }} className="relative mt-4 w-full min-h-14 px-4 py-2 border border-border rounded-md bg-secondary/15 cursor-pointer hover:bg-secondary/20 transition-colors">
                 <div className="flex justify-between items-center w-full h-full">
                     <span className="font-main font-semibold text-white text-lg">{startingPointName} →  {endingPointName}</span>
-                    <span className="font-main text-white/80 text-lg font-semibold">{totalFare}</span>
+                    <span className="font-main text-white/80 text-lg font-semibold">₱{totalFare}</span>
                 </div>
                 <div className="flex justify-between items-center w-full h-full mb-3">
                     <span className="font-main text-white/80">{routeName}</span>
-                    <span className="font-main text-white/80">{farePerPerson}/person</span>
+                    <span className="font-main text-white/80">₱{farePerPerson}/person</span>
                 </div>
 
                 <span className="font-main text-white/80 text-sm text-left block"> {passengerAmount} {type} passenger(s) · ₱{bill} bill</span>
