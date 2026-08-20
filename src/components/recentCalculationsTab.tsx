bf485834f79f1812d4cbda4653b3ac6bedeb3c2d
k-metra <metrakurt@gmail.com>
2026-08-21 00:29:39 +0800

    integrate calculation card with recentCalculationsTab to map out recent calculation history

--- src/components/recentCalculationsTab.tsx
+++ src/components/recentCalculationsTab.tsx
@@ -1,3 +1,55 @@
-export default function RecentCalculationsTab() {
-     
+import CalculationCard from "./calculationCard";
+
+import { type RecentCalculation } from "@utils/recentCalculations";
+
+type RecentCalculationsTabProps = {
+    calculations: RecentCalculation[];
+    onSelect: (calculation: RecentCalculation) => void;
+    onClear: () => void;
+}
+
+
+export default function RecentCalculationsTab({ calculations, onSelect, onClear }: RecentCalculationsTabProps) {
+     return (
+        <>
+            <div className=" my-10 mx-auto max-w-4xl max-h-96 h-full w-full border border-border rounded-md p-4">
+
+                <div className="w-full flex justify-between items-center mb-4 px-3">
+                    <div>
+                        <h2 className="font-main text-lg text-white font-semibold">Recent Calculations</h2>
+                        <span className='font-main text-white/80 text-sm mb-4'>Your latest fare calculations</span>
+                    </div>
+
+                    <button onClick={onClear} className="*:font-main text-secondary font-semibold cursor-pointer hover:underline">Clear All</button>
+                </div>
+
+                <hr className="border-t border-t-border w-[calc(100%+2rem)] mt-2 -mx-4 block"  />
+
+                {calculations?.length === 0 && (
+                    <div className="w-full h-full flex justify-center items-center">
+                        <span className="font-main text-white/80 text-sm text-center my-auto p-8">No recent calculations</span>
+                    </div>
+                )}
+
+                {calculations?.map((calculation) => (
+                    <CalculationCard 
+                        key={calculation.timestamp}
+                        startingPoint={calculation.startingPoint}
+                        endingPoint={calculation.endingPoint}
+                        routeId={calculation.route}
+                        passengerAmount={calculation.passengerAmount}
+                        type={calculation.type}
+                        bill={calculation.bill}
+                        farePerPerson={calculation.farePerPerson}
+                        totalFare={calculation.totalFare}
+                        timestamp={calculation.timestamp}
+                        onClick={onSelect}
+                    />
+                ))}
+
+               
+             
+            </div>
+        </>
+     )
 }
