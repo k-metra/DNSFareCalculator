2114d7e27548c9c88cb0c2bdb82d1a6174ab4a69
k-metra <metrakurt@gmail.com>
2026-08-21 00:27:47 +0800

    display calculation info via calculation card component

--- src/components/calculationCard.tsx
+++ src/components/calculationCard.tsx
@@ -1,40 +1,84 @@
 import formatElapsedTime from "@utils/formatElapsedTime";
+import type { RecentCalculation } from "@utils/recentCalculations";
+import { routes } from "../data/routes";
+import { motion, AnimatePresence } from "motion/react";
 
 type CalculationCardProps = {
     startingPoint: string;
     endingPoint: string;
-    route: string;
+    routeId: string;
     passengerAmount: number | string;
     type: string;
     bill: string;
     farePerPerson: number | string;
     totalFare: number | string;
     timestamp: number;
+    onClick?: (calculation: RecentCalculation) => void;
 }
 
 export default function CalculationCard({
-    startingPoint, endingPoint, route, passengerAmount, type, bill, farePerPerson, totalFare, timestamp
+    startingPoint, endingPoint, routeId, passengerAmount, type, bill, farePerPerson, totalFare, timestamp, onClick
 
     
 }: CalculationCardProps) {
 
-    
+    const routeData = routes.find((route) => route.id === routeId);
+
+    const routeName = routeData ? routeData.name : "Unknown Route";
+    const startingPointName = routeData ? routeData.stops.find((stop) => stop.id === startingPoint)?.name : "Unknown Start";
+    const endingPointName = routeData ? routeData.stops.find((stop) => stop.id === endingPoint)?.name : "Unknown Stop";
+
 
     return (
         <>
-            <button className="relative mt-4 w-full min-h-14 px-4 py-2 border border-border rounded-md bg-secondary/15 cursor-pointer hover:bg-secondary/20 transition-colors">
+            <motion.button initial="initial" whileHover="hover" onClick={() => {
+                if (onClick) {
+                    onClick(
+                        {
+                            startingPoint: startingPoint,
+                            endingPoint: endingPoint,
+                            route: routeId,
+                            passengerAmount: passengerAmount,
+                            type: type,
+                            bill: bill,
+                            farePerPerson: farePerPerson,
+                            totalFare: totalFare,
+                            timestamp: timestamp
+                        } as RecentCalculation
+                    )
+                }
+            }} className="relative mt-4 w-full min-h-14 px-4 py-2 border border-border rounded-md bg-secondary/15 cursor-pointer hover:bg-secondary/20 transition-colors">
                 <div className="flex justify-between items-center w-full h-full">
-                    <span className="font-main font-semibold text-white text-lg">{startingPoint} →  {endingPoint}</span>
-                    <span className="font-main text-white/80 text-lg font-semibold">₱{totalFare}</span>
+                    <span className="font-main font-semibold text-white text-lg">{startingPointName} →  {endingPointName}</span>
+                    <span className="font-main text-white/80 text-lg font-semibold">{totalFare}</span>
                 </div>
                 <div className="flex justify-between items-center w-full h-full mb-3">
-                    <span className="font-main text-white/80">{route}</span>
-                    <span className="font-main text-white/80">₱{farePerPerson}/person</span>
+                    <span className="font-main text-white/80">{routeName}</span>
+                    <span className="font-main text-white/80">{farePerPerson}/person</span>
                 </div>
 
                 <span className="font-main text-white/80 text-sm text-left block"> {passengerAmount} {type} passenger(s) · ₱{bill} bill</span>
                 <span className="font-main text-white/80 text-sm text-left block">{formatElapsedTime(timestamp)}</span>
-            </button>
+
+                <AnimatePresence>
+
+                    <motion.i 
+                        variants={{
+                            initial: { opacity: 0, y: "100%" },
+                            hover: { opacity: 1, y: "0%" },
+                            
+                        }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 300,
+                            damping: 25,
+                            opacity: { duration: 0.2 }
+                        }}
+                        className="text-2xl text-secondary fa-solid fa-arrow-up-right-from-square absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"></motion.i>
+
+                </AnimatePresence>
+
+            </motion.button>
         </>
     )
 }
